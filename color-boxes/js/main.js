8ef4d4abf6014c97ffed1ed7aa524b748060f28b
Veronika Pogrebna <veronika.pogrebna@capitalone.com>
2017-08-08 13:15:36 -0400

    updated notes

--- color-boxes/js/main.js
+++ color-boxes/js/main.js
@@ -8,6 +8,8 @@ $(function() {
   // DOM has loaded
   var BOX_COUNT = 12;
   var $container = $('.container');
+
+
   function getRandomColor() {
     var colorClasses = ['blue', 'green', 'red', 'yellow'];
     return colorClasses[Math.floor(Math.random() * colorClasses.length)];
@@ -17,14 +19,30 @@ $(function() {
     // can do method chaining, because with every call jquery returns itself
     var $box = $('<div>').addClass(`box ${getRandomColor()}`);
     var $innerBox = $('<div>').addClass('innerBox');
+    var $img = $('<img>')
+                      .attr('src', 'images/mike.png')
+                      .attr('alt', 'Mike')
+                      .addClass('faceImg');
+
+    $innerBox.append($img);
 
     $innerBox.on('click', function() {
       $(this).removeClass().addClass(`innerBox ${getRandomColor()}`);
     });
 
+    $innerBox.on('dblclick', function() {
+      $(this).fadeOut('slow', function(){
+        // The animation is complete
+      });
+    });
     $box.append($innerBox);
     $container.append($box);
   }
 
 
+  $('#google').on('click', function(e) {
+    alert('you clicked');
+    e.preventDefault();
+    return false;
+  })
 });
